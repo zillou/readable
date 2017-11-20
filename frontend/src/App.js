730666135600665994f9b6fbdf1bd629c7944a71
Derrick Zhang <derrick.zhang@welab.co>
2017-11-20 22:27:07 +0800

    Make App a functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch} from "react-router-dom"
 
 import Home from "./components/Home"
@@ -9,32 +9,29 @@ import About from "./components/About"
 import PostDetailView from "./components/PostDetailView"
 import PostForm from "./components/PostForm"
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <main className="container">
-            <div className="row">
-              <div className="col-md-9">
-                <Switch>
-                  <Route exact path="/" component={Home}/>
-                  <Route path="/about/" component={About} />
-                  <Route path="/posts/new" component={PostForm} />
-                  <Route exact path="/:category" component={Category}/>
-                  <Route exact path="/:category/:post_id" component={PostDetailView} />
-                </Switch>
-              </div>
-              <div className="col-md-3">
-                <Sidebar />
-              </div>
-            </div>
-          </main>
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <main className="container">
+        <div className="row">
+          <div className="col-md-9">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/about/" component={About} />
+              <Route path="/posts/new" component={PostForm} />
+              <Route exact path="/:category" component={Category}/>
+              <Route exact path="/:category/:post_id" component={PostDetailView} />
+            </Switch>
+          </div>
+
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
         </div>
-      </BrowserRouter>
-    );
-  }
-}
+      </main>
+    </div>
+  </BrowserRouter>
+)
 
 export default App;
